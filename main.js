5621daf54f52788934b7f25996b5e1e860016a21
Danylo Vovchok <vovchok.danya01@gmail.com>
2024-05-09 21:55:26 +0200

    define mongodb connection

--- main.js
+++ main.js
@@ -2,8 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const dashboardRoutes = require('./routes/dashboardRoutes');
+
+
 const app = express();
 const port = 3000;
+
+const { MongoClient } = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+const dbName = 'personalBudgetTracker';
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -16,6 +23,22 @@ app.use(authRoutes);
 
 app.use(dashboardRoutes);
 
+
+async function main() {
+  try {
+      await client.connect();
+      console.log("Connected successfully to MongoDB");
+      const db = client.db(dbName);
+      const transactionsCollection = db.collection('transactions');
+
+
+  } catch (e) {
+      console.error(e);
+  }
+}
+
+
+main().catch(console.error);
 
 app.get('/', (req, res) => {
   res.send(`
